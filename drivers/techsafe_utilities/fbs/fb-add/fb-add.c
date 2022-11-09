ed8d9c78c2817face8cf37fdb04629ab4c54ded1
Ege Evirgen <egevirgen@gmail.com>
2022-11-09 17:02:05 +0300

    feat(Techsafe): Change current working board from stm32f746g to native_posix_64

--- drivers/techsafe_utilities/fbs/fb-add/fb-add.c
+++ drivers/techsafe_utilities/fbs/fb-add/fb-add.c
@@ -1,8 +1,10 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/device.h>
 #include <zephyr/techsafe_utilities/ports/port-int/port-int.h>
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(fb_add, CONFIG_TECHSAFE_UTILITIES_LOG_LEVEL);
+
+#include <zephyr/kernel.h>
+//#include <zephyr/logging/log.h>
+//LOG_MODULE_REGISTER(fb_add, CONFIG_TECHSAFE_UTILITIES_LOG_LEVEL);
 
 #define DT_DRV_COMPAT techsafe_fb_add
 
@@ -23,16 +25,17 @@ struct fb_add_data {
 
 static int fb_add_init(const struct device *dev)
 {
-
     const struct fb_add_config *config = dev->config;
     struct fb_add_data *data = dev->data;
 
     port_int_get_value(config->input1, &data->value1);
     port_int_get_value(config->input2, &data->value2);
+
     data->output = data->value1 + data->value2;
     port_int_set_value(config->output, data->output);
 
-    LOG_DBG("FB Name = %s || IN1=%d IN2=%d OUT=%d", config->name, data->value1, data->value2, data->output);
+    printk("FB Name = %s || IN1=%d IN2=%d OUT=%d\n", config->name, data->value1, data->value2, data->output);
+//    LOG_DBG("FB Name = %s || IN1=%d IN2=%d OUT=%d", config->name, data->value1, data->value2, data->output);
 
     return 0;
 }
